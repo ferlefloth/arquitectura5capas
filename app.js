c45006f50b9df4a4bf5a74545cbf0d135eadd1d2
Mello <mellolefloth@gmail.com>
2022-04-17 20:36:30 -0300

    fix connecting the mongoDb

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ app.use(express.json())
 const userRoutes = require('./routes/user');
 app.use('/api/', userRoutes) // se puede usar como app.use('/api/', userRoutes) --->localhost:3000/api/home; Se puede usar muchas rutas para distintas funcionalidades
 
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
 
 //creating a server 
 app.listen('3000', () => {
     console.log('Server is running succesfully! :D ')
 });
 
-mongoose.connect('mongodb+srv://mello:123@cluster0.r7qsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true },()=>{
+mongoose.connect('<<<URL DEL CLUSTER DE MONGO>>>', { useNewUrlParser: true, useUnifiedTopology: true },()=>{
 
     console.log('DatabaseConnected')
     
